46257c21863b66f7d8ae31b20c30c3dc2eff0bb0
MJ Richardson <michael.james.richardson@gmail.com>
2012-11-16 13:16:33 +1000

    Initial commit of Parser

---
+++ TypeSql/Parser/ParseResult.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace TypeSql.Parser
+{
+    internal class ParseResult
+    {
+        public IEnumerable<OutputToken> OutputTokens { get; private set; }
+
+        public ParseResult(IEnumerable<OutputToken> outputTokens)
+        {
+            OutputTokens = outputTokens;
+        }
+    }
+
+    internal class OutputToken
+    {
+        public string Id { get; private set; }
+        public string Type { get; private set; }
+
+        public OutputToken(string id, string type )
+        {
+            Id = id;
+            Type = type;
+        }
+    }
+}
---
+++ TypeSql/Parser/Parser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TypeSql.Parser
+{
+    internal class Parser
+    {
+        public ParseResult Parse(string sql)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
