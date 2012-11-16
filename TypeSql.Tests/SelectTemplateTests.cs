338213e8d0b31a573abf9793c128ecb1e0d00ff0
Ben Priebe <benpriebe@gmail.com>
2012-11-16 15:51:19 +1000

    Added Async Processing for the Select Templates

--- TypeSql.Tests/SelectTemplateTests.cs
+++ TypeSql.Tests/SelectTemplateTests.cs
@@ -1,5 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TypeSql.Tests.Templates;
 
 namespace TypeSql.Tests
 {
@@ -9,9 +14,30 @@ namespace TypeSql.Tests
         [TestMethod]
         public void SimpleQuery_TypeArguments_NoConditions()
         {
-            var records = new ExampleSelectTemplate().Records;
-            Assert.IsNotNull(records);
-            Assert.IsTrue(records.Any());
+            var results = new ExampleSelectTemplate().Execute();
+            Assert.IsNotNull(results);
+            Assert.IsTrue(results.Any());
+        }
+
+        [TestMethod]
+        public void SimpleQuery_TypeArguments_NoConditions_Async()
+        {
+            var executeAsync = new ExampleSelectTemplate().ExecuteAsync();
+            var assertResults = executeAsync.ContinueWith((results, state) =>
+            {
+                IEnumerable<ExampleSelectTemplate.Result> records = results.Result;
+                Assert.IsNotNull(records);
+                Assert.IsTrue(records.Any());
+            }, true);
+            Task.WaitAll(executeAsync, assertResults);
+        }
+
+        [TestMethod]
+        public async Task SimpleQuery_TypeArguments_NoConditions_AsyncAwait()
+        {
+            var asyncResults = await new ExampleSelectTemplate().ExecuteAsync();
+            Assert.IsNotNull(asyncResults);
+            Assert.IsTrue(asyncResults.Any());
         }
 
         [TestMethod]
