6b6485fef429259e5594ae91f58b20596ea19ce4
mj.richardson <michael.james.richardson@gmail.com>
2012-11-18 12:15:58 +1000

    First-draft of using a T4 template to generate a class from a parse-result

---
+++ TypeSql.Tests/Templating/TemplatingTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using TypeSql.Parser;
+using TypeSql.Templating;
+using Xunit;
+
+namespace TypeSql.Tests.Templating
+{
+    public class TemplatingTests
+    {
+        //todo: this is just a dummy test, to quickly trial the templating. Remove me later.
+        [Fact]
+        public void CreateCSharpClassFromParseResult()
+        {
+           //arrange 
+            var parseResult = new ParseResult(
+                new List<OutputToken> { new OutputToken("Name", "string") },
+                new List<InputToken> { new InputToken("Id", "int") });
+
+            const string sqlName = "UserNameById";
+
+            var template = new CSharpTemplate(sqlName, parseResult);
+
+            //act
+            string result = template.TransformText();
+
+        }
+    }
+}
---
+++ TypeSql/Templating/CSharpTemplatePartial.cs
@@ -0,0 +1,16 @@
+﻿using TypeSql.Parser;
+
+namespace TypeSql.Templating
+{
+    internal partial class CSharpTemplate
+    {
+        private readonly string _name;
+        private readonly ParseResult _parseResult;
+
+        public CSharpTemplate(string name, ParseResult parseResult)
+        {
+            _name = name;
+            _parseResult = parseResult;
+        }
+    }
+}
