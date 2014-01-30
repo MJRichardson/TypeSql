27857ded54f4eb8ff80c7c2baf78df9135879547
mj.richardson <michael.james.richardson@gmail.com>
2014-01-31 06:49:28 +1000

    Exploring VB support

---
+++ VB.Sample/VbDao.vb
@@ -0,0 +1,24 @@
+﻿Imports TypeSql
+
+Public Class VbDao
+    Inherits DapperDao(Of Result)
+
+    Public Sub New(connectionString As String)
+        MyBase.New(connectionString)
+    End Sub
+
+    Public Sub New(connection As IDbConnection, Optional transaction As IDbTransaction = Nothing)
+        MyBase.New(connection, transaction)
+    End Sub
+
+    Public Overloads Function Execute(Optional blah As String = "", Optional buffered As Boolean = True) As IEnumerable(Of Result)
+        Dim parameters = New With {.blah = ""}
+        Return MyBase.Execute(parameters, buffered)
+    End Function
+
+    Protected Overrides ReadOnly Property Sql As String
+        Get
+
+        End Get
+    End Property
+End Class
